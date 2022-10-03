010800b74193e0d7908e048e92c17a6404b081dd
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2022-10-03 08:41:01 -0500

    cambios2

--- js/des.js
+++ js/des.js
@@ -1,4 +1,14 @@
 async function loadFile(file) {
-    let text = await file.text();
-    document.getElementById('archivo').textContent = texto;
+    let texto = await file.text();
+    document.getElementById('resultado').textContent = texto;
+//    console.log(texto)
+    var cifrado = CryptoJS.DES.encrypt(texto, "abcdefgh");
+    console.log(cifrado);
 }
+
+/*
+      async function loadFile(file) {
+        let text = await file.text();
+        document.getElementById('output').textContent = text;
+      }
+*/
