1ec1a449b522f8b58206569d3597f3a02ad347f5
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2022-10-04 22:52:23 -0500

    cambios5

--- js/des.js
+++ js/des.js
@@ -1,22 +1,39 @@
-/*
-async function loadFile(file) {
-    //optenemos valor del string
-    let texto = await file.text();
-    console.log(texto);
+function cifrado(){    
 
-}
-*/
-
-function cifrado(){
-    var clave=document.getElementById("clave");
+    var clave=document.getElementById("clave").value;
     console.log(clave);
-    console.log("hola");
-    
 }
+
 
 function descifrado(){
-    var clave=document.getElementById("clave");
-    console.log(clave);
-    console.log("hola");
-    
+    var clave=document.getElementById("clave").value;
+    console.log(clave);   
+}
+
+function leerArchivo()
+{
+    /*
+    var CryptoJS = require("crypto-js");
+
+    // Encrypt
+    var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
+
+    // Decrypt
+    var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
+    */
+    //var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+    //console.log(originalText); // 'my message'
+
+    var archivo=document.getElementById("file");
+    var reader = new FileReader();
+    console.log("Estas dentro de la funcion")
+    reader.readAsDataURL (archivo.files [0]); // Iniciar una solicitud asincrónica
+    reader.onload = function()
+    {
+        // Después de la lectura, los datos se guardan en el atributo de resultado del objeto
+        var texto=this.result;
+        console.log(texto)   
+    }
+    //console.log(texto);//aqui no sirve esto
 }
